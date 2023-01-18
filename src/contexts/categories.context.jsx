12897e71376e2f8e447638aed03c3a439834ce37
shahana <rafiyashahana@gmail.com>
2023-01-18 11:37:37 +0300

    added categoriesMap into CategoriesContext and also added categories-preview and its nested routes

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from "react";
+import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from "../utils/firebase.utils";
+
+export const CategoriesContext = createContext({
+  CategoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState([]);
+  /* to set data into firestore
+  useEffect(() => {
+    addCollectionAndDocuments("categories", SHOP_DATA);
+  }, []);
+*/
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
